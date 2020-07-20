9fb27761239f484f6f4e9ac383510368eacf02b8
yingjieweb <1184061078@qq.com>
2020-07-20 21:54:43 +0800

    add Profile.vue and router

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,41 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <Header></Header>
+
+    <div class="views">
+      <router-view/>
     </div>
-    <router-view/>
+
+    <Footer></Footer>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts">
+  import Vue from 'vue'
+  import {Component} from "vue-property-decorator";
 
-#nav {
-  padding: 30px;
+  import Header from './components/Header.vue'
+  import Footer from './components/Footer.vue'
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+  @Component({
+    components: {
+      Header,
+      Footer
     }
+  })
+
+  export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+  @import "~@/assets/styles/reset.scss";
+  @import "~@/assets/styles/helper.scss";
+
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
   }
-}
 </style>
---
+++ src/views/Profile.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="profile">
+    这是我的 Profile
+  </div>
+</template>
+
+<script lange="ts">
+  import Vue from 'vue'
+
+  export default class Profile extends Vue{
+
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
