6e9dc6e19767b12a6054fcf000e694cae22b7a40
yingjieweb <1184061078@qq.com>
2020-07-20 21:54:15 +0800

    add Header/Footer comp

---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script lange="ts">
+  export default {
+    name: "Footer"
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script lange="ts">
+  export default {
+    name: "Header"
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
