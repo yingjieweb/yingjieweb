597a0f42d25e82ccba4a2f5d59c658f1df3d2e5f
yingjieweb <1184061078@qq.com>
2020-07-23 21:48:15 +0800

    add Social comp

---
+++ src/views/Profile/Social.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul class="social">
+    <li v-for="item in social" :key="item.link">
+      <svg class="icon" aria-hidden="true">
+        <use :xlink:href="item.icon"></use>
+      </svg>
+      <a :href="item.link" target="_blank">{{item.link}}</a>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+  import '@/assets/icons/index'
+  import {Component} from 'vue-property-decorator'
+
+  @Component
+  export default class Social extends Vue {
+    social = [
+      {label: '邮箱', icon: '#icon-mail', link: '1184061078@qq.com'},
+      {label: 'CSDN', icon: '#icon-csdn', link: 'https://blog.csdn.net/marker__'},
+      {label: 'Github', icon: '#icon-github', link: 'https://github.com/yingjieweb'}
+    ]
+  }
+</script>
+
+<style scoped lang="scss">
+  .social {
+    a {
+      margin-left: 8px;
+      color: #409EFF;
+    }
+  }
+</style>
