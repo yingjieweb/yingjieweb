832e5ec777aaee33b66d4d3d4eb9b9fe54197a6d
yingjieweb <1184061078@qq.com>
2020-07-28 13:15:41 +0800

    add projectDB info and type

--- src/@type/global.d.ts
+++ src/@type/global.d.ts
@@ -3,3 +3,20 @@ type TMenuItem = {
   label: string
   icon: string
 }
+
+type TBlogItem = {
+  title: string
+  desc: string
+  avatar: string
+  url: string
+  date: string
+}
+
+type TProjectItem = {
+  title: string
+  desc: string
+  url: string
+  github: string
+  languages: string[]
+  stars: number
+}
---
+++ src/database/project.ts
@@ -0,0 +1,36 @@
+const projectDB: TProjectItem[] = [
+  {
+    title: '英雄联盟 UI 库',
+    desc: '一个基于 Vue.js 开发的英雄联盟主题 UI 组件库。纪念当年通宵打英雄联盟的大学时光。',
+    github: 'https://github.com/yingjieweb/league-ui',
+    document: 'http://yingjiesongi.gitee.io/league-ui-doc/#/intro',
+    languages: ['Vue'],
+    stars: 0
+  },
+  {
+    title: '小本本记账',
+    desc: '一款基于 Vue 全家桶 和 TypeScript 的单页面极简记账应用。',
+    github: 'https://github.com/yingjieweb/charge',
+    document: 'http://yingjiesongi.gitee.io/charge/#/money',
+    languages: ['Vue', 'TypeScript'],
+    stars: 1
+  },
+  {
+    title: '学术导航站点',
+    desc: '一款基于 jQuery 的前端学术导航站点，同时做了 PC 端和移动端适配。',
+    github: 'https://github.com/yingjieweb/navigator',
+    document: 'http://yingjiesongi.gitee.io/navigator/dist/index.html',
+    languages: ['jQuery'],
+    stars: 0
+  },
+  {
+    title: 'dombox 插件库',
+    desc: '基于原生 JS 封装的 JS/jQuery 插件库 (目前维护了 24 个函数接口)。',
+    github: 'https://github.com/yingjieweb/dombox',
+    document: 'https://github.com/yingjieweb/dombox#build-your-own-dom-api',
+    languages: ['JavaScript', 'jQuery'],
+    stars: 0
+  }
+]
+
+export default projectDB
