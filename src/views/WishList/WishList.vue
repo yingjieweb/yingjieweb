56d9f55607235ccf3f5729dc24c711373aefae89
yingjieweb <1184061078@qq.com>
2020-07-31 08:12:39 +0800

    basically implement WishList

--- src/views/WishList/WishList.vue
+++ src/views/WishList/WishList.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="wish-list">
-    <h1>愿望清单</h1>
-    <p>正在开发中...</p>
+    <h1>
+      <span>人生清单</span>
+
+      <span>
+        <el-radio v-model="type" label="done">已完成</el-radio>
+        <el-radio v-model="type" label="todo">还在立Flag</el-radio>
+      </span>
+    </h1>
+
+    <ul class="bucket-list-content">
+      <wish-list-item v-for="item in filterWishList" :item="item" :key="item.title"></wish-list-item>
+    </ul>
   </div>
 </template>
 
@@ -9,10 +19,35 @@
   import Vue from 'vue'
   import {Component} from "vue-property-decorator";
 
-  @Component
-  export default class WishList extends Vue {}
+  import WishListItem from "@/views/WishList/WishListItem.vue";
+  import wishList from "@/database/wishListDB";
+
+  @Component({
+    components: {WishListItem}
+  })
+  export default class WishList extends Vue {
+    type = 'done'
+
+    get filterWishList() {
+      return wishList.filter(item => item.status === this.type)
+    }
+  }
 </script>
 
 <style lang="scss" scoped>
+  .wish-list {
+    h1 {
+      display: flex;
+      justify-content: space-between;
 
+      span {
+        display: flex;
+        align-items: center;
+      }
+    }
+
+    &-image {
+      text-align: center;
+    }
+  }
 </style>
