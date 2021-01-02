07bbaa037a3bced3ce928cb6c1ad0367316136c6
yingjieweb <1184061078@qq.com>
2021-01-02 17:36:26 +0800

    update remote repository url

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:yingjieweb/resume.git master:gh-pages
+git push -f git@github.com:yingjieweb/yingjieweb.git master:gh-pages
 
 cd -
